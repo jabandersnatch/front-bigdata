717f1c1cff81987fd2a079c86369a8017d4b9654
jabandersnatch <ja.mendez@uniandes.edu.co>
2023-04-20 10:41:09 -0500

    Initial commit

---
+++ src/models/req1.ts
@@ -0,0 +1,19 @@
+import { Document, Schema, model } from "mongoose";
+
+interface IReq1 extends Document {
+  STATE: string;
+  year: number;
+  month: number;
+  count: number;
+}
+
+const Req1Schema = new Schema<IReq1>({
+  STATE: { type: String, required: true },
+  year: { type: Number, required: true },
+  month: { type: Number, required: true },
+  count: { type: Number, required: true },
+});
+
+const Req1Model = model<IReq1>("Req1_2017_1", Req1Schema);
+
+export default Req1Model;
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import DBconnect from '@/utils/dbconnect'
+import Req1Model from '@/models/req1'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+    if ( req.method === 'POST' ) {
+    await DBconnect()
+    const req1 = new Req1Model({
+      STATE: 'CA',
+      year: 2020,
+      month: 1,
+      count: 1,
+    })
+    await req1.save();
+    res.status(200).json(req1)
+  } else if ( req.method === 'GET' ) {
+    await DBconnect()
+    const req1s = await Req1Model.find({})
+    res.status(200).json(req1s)
+  } else {
+    res.status(400).json({ message: 'Bad Request' })
+  }
 }
